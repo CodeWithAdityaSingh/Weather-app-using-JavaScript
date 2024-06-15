4d345b92d871e8e149906b0f0f72a95a0eb0a9cf
Aditya Singh <adityasingh161020003@gmail.com>
2024-06-16 00:02:46 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+async function fech(city) {
+
+
+
+    try {
+
+        let a = await fetch('http://api.weatherapi.com/v1/current.json?key=1fdad4d08748409dbbe64954241506&q=' + city + ' &aqi=no')
+
+        let c = await a.json()
+         
+        
+
+        document.querySelector('#location').innerHTML = c.location.name
+
+        document.querySelector('#degree').innerHTML = c.current.temp_c + `&degC`
+
+        document.querySelector('#place').innerHTML = c.location.country
+
+        document.querySelector('#windspeed').innerHTML = c.current.
+            wind_kph + "km/h"
+
+        document.querySelector('#humidity ').innerHTML = c.current.
+            humidity + "%"
+
+        document.querySelector('#condition').innerHTML = c.current.condition.text
+
+
+
+        setInterval(() => {
+
+
+            let date = new Date()
+
+
+            document.querySelector('#day').innerHTML = date.toLocaleTimeString()
+        }, 1000)
+
+
+    } catch (error) {
+        console.log(error)
+
+
+    }
+
+}
+
+
+document.querySelector('button').addEventListener('click',()=>{
+   let a =  document.querySelector('input').value
+   fech(a)
+})
